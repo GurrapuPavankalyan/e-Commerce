fe9b56bb390adc60d94edbd21231fd4374347856
GurrapuPavanKalyan <pavankalyan.gurram@123>
2025-02-07 14:07:59 +0530

    Added IncrementQuantity, DecrementQuantity and RemoveItem reducers for buttons in the cart

--- src/Utils/cartSlice.js
+++ src/Utils/cartSlice.js
@@ -7,11 +7,32 @@ const cartSlice = createSlice({
     },
     reducers: {
         addToCart: (state, action) => {
-            state.items.push(action.payload);
+            const existingItem = state.items.find((item) => item.id === action.payload.id);
+            if(!existingItem) {
+                state.items.push(action.payload);
+            } else {
+                existingItem.quantity += 1;
+            }
+        },
+        incrementQuantity:  (state, action) =>  {
+            const item = state.items.find((item) => item.id === action.payload.id);
+            item.quantity += 1;
+        },
+        decrementQuantity:  (state, action) =>  {
+            const item = state.items.find((item) => item.id === action.payload.id);
+
+            if(item.quantity > 1) {
+                item.quantity -= 1;
+            } else {
+                state.items = state.items.filter((item) => item.id !== action.payload.id);
+            }         
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter((item) => item.id !== action.payload.id);
         },
     },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, incrementQuantity, decrementQuantity, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
